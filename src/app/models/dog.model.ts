2e4b59f289e2a83806082316e19d797c455b1757
JJAscencio <jhon0293@gmail.com>
2020-10-13 19:33:02 -0700

    add bootstrap, models and profile page to tabs

---
+++ src/app/models/dog.model.ts
@@ -0,0 +1,15 @@
+export interface Dog {
+    id: String,
+    name: String,
+    sex: String,
+    size: String,
+    breed: String,
+    age: String,
+    fplace: String,
+    found: String,
+    description: String,
+    collar: String,
+    status: String,
+    profilepic: String,
+    idPerrera: String
+}
--- src/app/tabs/profile/profile-routing.module.ts
+++ src/app/tabs/profile/profile-routing.module.ts
@@ -1,16 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { TabsPage } from './tabs.page';
+import { ProfilePage } from './profile.page';
 
 const routes: Routes = [
   {
     path: '',
-    component: TabsPage
-  },
-  {
-    path: 'profile',
-    loadChildren: () => import('./profile/profile.module').then( m => m.ProfilePageModule)
+    component: ProfilePage
   }
 ];
 
@@ -18,4 +14,4 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
-export class TabsPageRoutingModule {}
+export class ProfilePageRoutingModule {}
