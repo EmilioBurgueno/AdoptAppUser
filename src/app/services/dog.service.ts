71dd14d21de291c5692c9e5427d0246597850b44
Luis Enrique Hernandez Sandoval <luisenrique@mbp-de-luis-v2.lan>
2020-12-08 15:26:10 -0800

    Funcion sendApplication

--- src/app/services/dog.service.ts
+++ src/app/services/dog.service.ts
@@ -12,24 +12,24 @@ export class DogService {
   firestore: any;
 
   constructor(private afs: AngularFirestore,
-    private afsStorage: AngularFireStorage,
-    ) { }
+              private afsStorage: AngularFireStorage) { }
 
   async createDog(dog: any, profilepic: File) {
     return new Promise(async (resolve, reject) => {
       try {
-        const dogId = this.afs.createId();
-        const filePath = `dogs/${dogId}/profilepic.jpeg`;
-
-        dog.id = dogId;
-        await this.uploadDogImage(dog,profilepic);
         await this.afs.firestore.runTransaction(async transaction => {
+          const dogId = this.afs.createId();
           const dogRef = this.afs.doc(`dogs/${dogId}`).ref;
-          console.log(filePath)
+          dog.id = dogId;
+          if (profilepic != null) {
+            const filePath = `dogs/${dogId}/profilepic.jpeg`;
 
-         dog.profilepic = await this.afsStorage.ref(filePath).getDownloadURL().toPromise()
+            await this.uploadDogImage(dog, profilepic);
 
-          transaction.set(dogRef,dog);
+            dog.profilepic = await this.afsStorage.ref(filePath).getDownloadURL().toPromise();
+
+          }
+          transaction.set(dogRef, dog);
         });
         resolve(true);
       } catch (error) {
@@ -38,10 +38,31 @@ export class DogService {
     });
   }
 
+  // async uploadDogImage(dog: any, profilepic: File) {
+  //   return new Promise(async (resolve, reject) => {
+  //     try {
+  //       // ERROR POR EL PATH?
+  //       const filePath = `dogs/${dog.id}/profilepic.jpeg`;
+  //       const task = this.afsStorage.upload(filePath, profilepic);
+  //       await task.snapshotChanges().toPromise();
+  //       const pictureUrl = await this.afsStorage.ref(filePath).getDownloadURL().toPromise();
+  //       console.log(pictureUrl);
+  //       await this.updateDog(dog, { pictureUrl });
+
+  //       resolve(true);
+  //     } catch (error) {
+  //       reject(error);
+  //     }
+  //   });
+  // }
+
   uploadDogImage(dog: any, profilepic: File) {
     const filePath = `dogs/${dog.id}/profilepic.jpeg`;
+    //console.log('1a');
     const task = this.afsStorage.upload(filePath, profilepic);
-
+    //console.log('log del upload image');
+    //console.log(filePath);
+    //console.log(task);
     return task.snapshotChanges().toPromise();
   }
 
@@ -49,9 +70,13 @@ export class DogService {
     return new Promise(async (resolve, reject) => {
       try {
         const filePath = `dogs/${dId}/profilepic.jpeg`;
+        //console.log('5');
         const task = this.afsStorage.ref(filePath).delete();
+        //console.log('6');
         await task.toPromise();
-        await this.updateDog(dId, { pictureUrl: null });
+        //console.log('7');
+        await this.updateDog(dId, { profilepic: null }, null);
+        //console.log('8');
 
         resolve(true);
       } catch (error) {
@@ -72,11 +97,38 @@ export class DogService {
 
         return { id, ...dog } as Dog;
       }))
-    )
+    );
   }
 
-  updateDog(dId: string, updatedDog: any) {
-    return this.afs.doc(`dogs/${dId}`).update(updatedDog);
+  updateDog(dId: string, updatedDog: any, profilepic: File) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        if (profilepic != null) {
+          const filePath = `dogs/${dId}/profilepic.jpeg`;
+
+          updatedDog.id = dId;
+          //console.log('9');
+          await this.uploadDogImage(updatedDog, profilepic);
+          //console.log('10');
+          await this.afs.firestore.runTransaction(async transaction => {
+            //console.log('11');
+            const dogRef = this.afs.doc(`dogs/${dId}`).ref;
+            //console.log(filePath);
+
+            updatedDog.profilepic = await this.afsStorage.ref(filePath).getDownloadURL().toPromise();
+            //console.log('12');
+
+            transaction.set(dogRef, updatedDog);
+            //console.log('13');
+          });
+        }
+        this.afs.doc(`dogs/${dId}`).update(updatedDog);
+        //console.log('14');
+        resolve(true);
+      } catch (error) {
+        reject(error);
+      }
+    });
   }
 
   deleteDog(dId: string) {
--- src/app/tabs/modals/adoption/adoption.page.ts
+++ src/app/tabs/modals/adoption/adoption.page.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { ModalController, NavController } from '@ionic/angular';
+import { AlertController, LoadingController, ModalController, NavController } from '@ionic/angular';
 import { AuthService } from 'src/app/services/auth.service';
 import { DogService } from 'src/app/services/dog.service';
 import { UserService } from 'src/app/services/user.service';
@@ -16,11 +16,17 @@ export class AdoptionPage implements OnInit {
   user: any;
   dogId: string;
   userId: string;
+  test: any[];
+  file: any;
+  loadingIndicator;
+  loading = false;
 
   constructor(private dogService: DogService,
     private authService: AuthService,
     private navCtrl: NavController,
-    private modalCtrl: ModalController) { }
+    private modalCtrl: ModalController,
+    private alertCtrl: AlertController,
+    private loadingCtrl: LoadingController) { }
 
   ngOnInit() {
     this.getDog(this.dID);
@@ -42,9 +48,63 @@ export class AdoptionPage implements OnInit {
     await this.modalCtrl.dismiss();
   }
 
-  sendApplication() {
-    //this.dog.adoptees.push(this.user.name)
-    console.log(this.user)
+  async presentLoading(body: string) {
+    this.loadingIndicator = await this.loadingCtrl.create({
+      message: body
+    });
+    this.loading = true;
+    await this.loadingIndicator.present();
+  }
+
+  async dismissLoading() {
+    this.loading = false;
+    await this.loadingIndicator.dismiss();
+  }
+
+  async presentAlert(title: string, body: string) {
+    const alert = await this.alertCtrl.create({
+      header: title,
+      message: body,
+      buttons: ['Listo']
+    });
+
+    await alert.present();
+  }
+
+  async presentAlertConfirm(title: string, body: string) {
+    const alert = await this.alertCtrl.create({
+      header: title,
+      message: body,
+      buttons: [
+        {
+          text: 'Listo',
+          handler: () => {
+            this.closeModal();
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
+  async sendApplication() {
+    await this.presentLoading('Guardando el perfil...');
+    this.test = this.dog.adoptees
+    this.test.push(this.user.id)
+    const updatedDog = {
+      adoptees: this.test
+    };
+    console.log(updatedDog);
+    try {
+      await this.dogService.updateDog(this.dog.id.toString(), updatedDog, this.file);
+      this.dismissLoading();
+      this.presentAlertConfirm('¡Exito!', 'El perfil ha sido modificado exitosamente.');
+      this.closeModal();
+    } catch (error) {
+      this.dismissLoading();
+      this.presentAlert('Algo malo ha pasado', error.message);
+    }
   }
 
 }
